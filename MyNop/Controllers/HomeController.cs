8f5bb72de6b464edba960a3698372efd05e8742b
NickJane <mycooldog@foxmail.com>
2016-02-25 16:35:01 +0800

    提交

---
+++ MyNop/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nop.Data;
+
+namespace MyNop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            NopObjectContext a = new NopObjectContext();
+            var name = a.Set<Nop.Core.Domain.UserAccount>().FirstOrDefault().UserName;
+            name = a.Set<Nop.Core.Domain.UserAccount>().FirstOrDefault().Auth_Roles.FirstOrDefault().RoleName;
+            return Content("");
+        }
+	}
+}
--- Nop.Core/Class1.cs
+++ Nop.Core/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nop.Data
+namespace Nop.Core
 {
     public class Class1
     {
